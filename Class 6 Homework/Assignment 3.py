de0936aaa4c6896f3e574169ef648e680c5c319b
Om Joshi <omdjoshi45@gmial.com>
2025-12-26 16:25:16 +1100

    Class 6 Homework

---
+++ Class 6 Homework/Assignment 3.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self):
+        pass
+    def eat(self):
+        print("Animal is eating.")
+
+class Dog(Animal):
+    def __init__(self):
+        super().__init__()
+    def bark(self):
+        print("Dog is barking!")
+
+class Cat(Animal):
+    def __init__(self):
+        super().__init__()
+    def meow(self):
+        print("Cat says meow!")
+
+dog = Dog()
+cat = Cat()
+dog.eat()
+cat.eat()
+dog.bark()
+cat.meow()
