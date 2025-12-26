58a815e643c5fd64c26efff0e857ee3fa099a408
Om Joshi <omdjoshi45@gmial.com>
2025-12-26 15:11:12 +1100

    Inheritance

--- Class 6/Inheritance.py
+++ Class 6/Inheritance.py
@@ -16,3 +16,21 @@ print(a.x)
 b = B()
 b.f1()
 print(b.x)
+
+# parent/super/base class
+class Parent:
+    def __init__(self,n):
+        self.name = n
+        print("parent object is created")
+    def f1(self):
+        print("f1 from parent class is called")
+# derived class, sub class
+class Child(Parent):
+    def __init__(self,n,x):
+        super().__init__(n)
+        self.x = x
+        print("child class object is created")
+    def f2(self):
+        print("f2 from class child")
+# p = Parent("xyz")
+c = Child("xyz",100)
