768e8eaf682b24b707cb743aa0a1d3c780e3cfc4
Om Joshi <omdjoshi45@gmial.com>
2026-03-13 18:28:32 +1100

    Finished dino game but database left

--- Pygame classes/Dino game/Dino game.py
+++ Pygame classes/Dino game/Dino game.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,random
 pygame.init()
 pygame.mixer.music.load(r"Pygame classes/Dino game/dino music.mp3")
 pygame.mixer.music.play(-1)
@@ -18,6 +18,14 @@ pygame.display.set_caption("Dino game")
 dinoY = h-210
 dinoX = 100
 ground = h-150
+obstacle_list = [r"Pygame classes/Dino game/cactus.png", r"Pygame classes/Dino game/spikes.png"]
+obstacle_x = w-10
+obstacle_y = ground-50
+game_on_pause = True
+spawn_obstacle = pygame.USEREVENT + 1
+pygame.time.set_timer(spawn_obstacle,1500)
+score = 0
+score_font = pygame.font.Font(None,30)
 
 # Sprites -->
 class Dino(pygame.sprite.Sprite):
@@ -36,7 +44,7 @@ class Dino(pygame.sprite.Sprite):
         self.dino_on_ground = True
         self.dino_velocity_y = 0
         self.gravity = 1
-        self.jump_power = -20
+        self.jump_power = -25
     
     def load_images(self):
         for i in range(1,9):
@@ -117,10 +125,29 @@ class Dino(pygame.sprite.Sprite):
             self.jump()
         elif self.dino_state == "dead":
             self.death()
-    
+
+class Obstacles(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(random.choice(obstacle_list))
+        self.image = pygame.transform.scale(self.image,(70,100))
+        self.rect = self.image.get_rect(center = (obstacle_x,obstacle_y))
+        self.speed = 4
+
+    def move_left(self):
+        global score
+        self.rect.x -= self.speed
+        if self.rect.right< dino.rect.left:
+            self.kill()
+            score +=1
+
+    def update(self):
+        if game_on_pause == False:
+            self.move_left()
     
 dino = Dino()
 player_group = pygame.sprite.Group()
+obstacle_group = pygame.sprite.Group()
 player_group.add(dino)
 
 runninStatus = True
@@ -132,12 +159,23 @@ while runninStatus:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_p:
                 dino.dino_state = "run"
+                game_on_pause = False
             if event.key == pygame.K_SPACE:
                 dino.start_jump()
+                game_on_pause = False
             if event.key == pygame.K_d:
                 dino.dino_state = "dead"
+                game_on_pause = True
+        if event.type == spawn_obstacle and game_on_pause == False:
+            obstacle_group.add(Obstacles())
+
+    # Collision -->
+    if pygame.sprite.spritecollide(dino,obstacle_group,False,pygame.sprite.collide_mask):
+        game_on_pause = True
+        dino.dino_state = "dead"
+
     # Scroll Logic -->
-    if dino.dino_state != "idle":
+    if game_on_pause == False:
         groundx -= groundSpeed
         if groundx <= -102:
             groundx = 0
@@ -146,6 +184,10 @@ while runninStatus:
     screen.blit(ground_img,(groundx,h-150))
     player_group.draw(screen)
     player_group.update()
+    obstacle_group.draw(screen)
+    obstacle_group.update()
+    score_text = score_font.render(f"score : {score}",False,"RED","WHITE")
+    screen.blit(score_text,(10,10))
 
     pygame.display.update()
 
